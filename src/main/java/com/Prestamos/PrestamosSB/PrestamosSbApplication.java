4c400488220d3fe9a1ec0e3a1ddc7527cbc7fc4e
Saul Burgos <saulburgos6@gmail.com>
2023-03-04 22:16:25 -0400

    init project and implement controller health-check

---
+++ src/main/java/com/Prestamos/PrestamosSB/PrestamosSbApplication.java
@@ -0,0 +1,13 @@
+package com.Prestamos.PrestamosSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Prestamos/PrestamosSB/PrestamosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prestamos.PrestamosSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
