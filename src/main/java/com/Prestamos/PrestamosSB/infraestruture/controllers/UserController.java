dd1b36fae459a6f68a6ec1836e74732ee8c08281
Saul Burgos <saulburgos6@gmail.com>
2023-03-07 21:17:48 -0400

    implement Controller and test the users

--- src/main/java/com/Prestamos/PrestamosSB/infraestruture/controllers/UserController.java
+++ src/main/java/com/Prestamos/PrestamosSB/infraestruture/controllers/UserController.java
@@ -1,24 +1,43 @@
 package com.Prestamos.PrestamosSB.infraestruture.controllers;
 
+import com.Prestamos.PrestamosSB.application.create.UserCreator;
+import com.Prestamos.PrestamosSB.application.find.FindUser;
+import com.Prestamos.PrestamosSB.domain.User;
+import com.Prestamos.PrestamosSB.infraestruture.Dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
 
+    private final UserCreator userCreator;
+    private final FindUser findUser;
+
+    public UserController(UserCreator userCreator, FindUser findUser) {
+        this.userCreator = userCreator;
+        this.findUser = findUser;
+    }
+
     @GetMapping("/user")
-    public ResponseEntity<String> getUser(){
-        return new ResponseEntity<>("users", HttpStatus.OK);
+    public ResponseEntity<List<User>> getUser(){
+       List<User> userList =  findUser.findAllUser();
+        return new ResponseEntity<>(userList, HttpStatus.OK);
 
     }
 
-//    @PostMapping("/user")
-//    public ResponseEntity<HttpStatus> createUser(){
-//        return new ResponseEntity<>(HttpStatus.CREATED);
-//
-//    }
+    @PostMapping("/user")
+    public ResponseEntity<HttpStatus> createUser(@RequestBody UserDto user){
+        User newUser =  user.getUserFromDto();
+        userCreator.create(newUser);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
 }
--- src/test/java/com/Prestamos/PrestamosSB/infraestruture/controllers/UserControllerTest.java
+++ src/test/java/com/Prestamos/PrestamosSB/infraestruture/controllers/UserControllerTest.java
@@ -1,11 +1,16 @@
 package com.Prestamos.PrestamosSB.infraestruture.controllers;
 
+import com.Prestamos.PrestamosSB.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,14 +25,19 @@ class UserControllerTest {
 
     @Test
    public void getUser() {
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:"+ port + "/user",String.class);
+        ResponseEntity<User[]> response = restTemplate.getForEntity("http://localhost:"+ port + "/user",User[].class);
+
+
+
+        System.out.println(Arrays.toString(response.getBody()));
         assertEquals(HttpStatus.OK,response.getStatusCode());
-        assertEquals("users",response.getBody());
+
 
 
     }
 
     @Test
-    void createUser() {
+    void testCreateUser() {
+
     }
 }
