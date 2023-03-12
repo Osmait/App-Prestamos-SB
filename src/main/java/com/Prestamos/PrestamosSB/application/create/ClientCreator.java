15a76f18ee62439647f57f0197d4aab26a372852
Saul Burgos <saulburgos6@gmail.com>
2023-03-11 21:00:51 -0400

    implement service find all clients

--- src/main/java/com/Prestamos/PrestamosSB/application/create/ClientCreator.java
+++ src/main/java/com/Prestamos/PrestamosSB/application/create/ClientCreator.java
@@ -1,5 +1,17 @@
 package com.Prestamos.PrestamosSB.application.create;
 
+import com.Prestamos.PrestamosSB.domain.Client;
+import com.Prestamos.PrestamosSB.domain.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientCreator {
 
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public  void  create(Client client){
+        clientRepository.save(client);
+    }
 }
---
+++ src/test/java/com/Prestamos/PrestamosSB/application/create/ClientCreatorTest.java
@@ -0,0 +1,34 @@
+package com.Prestamos.PrestamosSB.application.create;
+
+import com.Prestamos.PrestamosSB.domain.Client;
+import com.Prestamos.PrestamosSB.domain.ClientRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ClientCreatorTest {
+
+    @MockBean
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private  ClientCreator clientCreator;
+
+
+
+    @Test
+    void create() {
+        Client client = new Client("saul", "burgos","saulburgos6@gmail.com", "12345678");
+        clientCreator.create(client);
+
+        Mockito.verify(clientRepository,Mockito.times(1)).save(client);
+
+
+
+    }
+}
